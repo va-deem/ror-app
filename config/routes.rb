9345cd7539d4c20f8f89833d12c7384b935df2c3
Vadim S <vadim.stebakov@gmail.com>
2020-11-12 20:31:34 +0300

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
-  root 'application#hello'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'static_pages/about'
+  get 'static_pages/contact'
 end
