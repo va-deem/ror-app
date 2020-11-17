e97e4065f39d8bdef97e0e914331160648f3c27d
Vadim S <vadim.stebakov@gmail.com>
2020-11-17 22:18:41 +0300

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,15 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password]) # or if user && user...
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user&.authenticate(params[:session][:password]) # or if user && user...
       # Log the user in and redirect to the user's show page.
-      log_in user
-      redirect_to user # user_url(user)
+      log_in @user # SessionsHelper
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user # user_url(user)
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
@@ -15,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { email.downcase! } #  or { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }
@@ -10,9 +11,32 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
 
   # Returns the hash digest of the given string.
-  def User.digest(string)
+  def self.digest(string) # or User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def self.new_token # or User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token) # not the same remember_token as the accessor
+    return false if remember_digest.nil?
+    # is_password? is boolean method to perform the comparison
+    # self.remember_digest is created by AR based on the name of the corresponding db column:
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
