d8fe88ef539530e6dea26bee98cfd40b330d15aa
Vadim S <vadim.stebakov@gmail.com>
2020-11-19 20:42:57 +0300

    Add account activation

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem "bootstrap-sass", "~> 3.4"
 gem "faker", "~> 2.14"
 gem "will_paginate", "~> 3.3"
 gem "bootstrap-will_paginate", "~> 1.0"
+gem "dotenv-rails", "~> 2.7"
 
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
@@ -24,8 +25,6 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
-# Use Active Model has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,12 @@ class UsersController < ApplicationController
 
   def index
     # @users = User.all
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated
   end
 
   def new
@@ -19,9 +20,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = 'Welcome to the Sample App!'
-      redirect_to @user # or redirect_to user_url(@user)
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+      # log_in @user
+      # flash[:success] = 'Welcome to the Sample App!'
+      # redirect_to @user # or redirect_to user_url(@user)
     else
       render 'new'
     end
