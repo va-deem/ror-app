6828c5a04ed14b429519647958de66d322c735ea
Vadim S <vadim.stebakov@gmail.com>
2020-11-20 22:43:08 +0300

    Add microposts to UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token
 
   before_save   :downcase_email
@@ -51,6 +52,12 @@ class User < ApplicationRecord
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
